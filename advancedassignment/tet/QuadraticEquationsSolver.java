c1c64d8e7b4e32505af54aefa8be6e2f597e29bb
Grizmo2610 <hoangtuantu893@gmail.com>
2024-02-25 19:24:57 +0700

    Fix Tet assignment

--- advancedassignment/tet/QuadraticEquationsSolver.java
+++ advancedassignment/tet/QuadraticEquationsSolver.java
@@ -13,13 +13,13 @@ public class QuadraticEquationsSolver {
 		return b * b - 4 * a * c;
 	}
 
-	public static boolean validEquation(double a, double b, double c, double delta){
+	public static boolean validEquation(double a, double delta){
 		return a != 0 && delta >= 0;
 	}
 
 	public static void solve(double a, double b, double c){
 		double delta = delta(a, b, c);
-		if (validEquation(a, b, c, delta)){
+		if (validEquation(a, delta)){
 			if (delta > 0){
 				double x1 = (-b + Math.sqrt(delta)) / (2 * a);
 				double x2 = (-b - Math.sqrt(delta)) / (2 * a);
